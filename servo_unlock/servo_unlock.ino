642fd8b47bd79600189734799113e847564d7743
Mark Aplet <mark@visual28.com>
2019-12-17 07:47:19 -0800

    improves accuracy of servo test

--- servo_unlock/servo_unlock.ino
+++ servo_unlock/servo_unlock.ino
@@ -6,26 +6,43 @@
 // Servo Library
 #include <Servo.h>
 
+#define BUTTON_PIN        2  // Button
+#define DELAY            20  // Delay per loop in ms
+
 Servo LockServo;  // create servo object to control a servo
 int POS = 0;      // variable to store the servo position
 int LOCKED = 1;   // default lock state
 
+
+boolean handle_button(){
+  int button_pressed = !digitalRead(BUTTON_PIN); // pin low -> pressed
+  return button_pressed;
+}
+
+
 void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH); // pull-up
+  Serial.begin(9600);
+  
    LockServo.attach(9);  // attaches the servo on pin 9 to the servo object
    LockServo.write(0);
    Serial.begin(9600);
 }
 
 void loop() {
-  if (LOCKED == 1) {
-    Serial.print("unlocking");
-    LockServo.write(100);
-    delay(2000);
-    LOCKED = 0;
-  } else if (LOCKED == 0) {
-    Serial.print("locking");
-    LockServo.write(0);
-    delay(2000);
-    LOCKED = 1;
+   boolean button_pressed = handle_button();
+
+   switch (button_pressed) {
+    case 1:
+      LockServo.write(120);
+      break;
+    case 0:
+      LockServo.write(0);
+      break;
   }
+
+ 
+  
+  delay(DELAY);
 }
